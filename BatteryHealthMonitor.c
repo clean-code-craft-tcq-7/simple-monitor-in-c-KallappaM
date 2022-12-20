f5282ca6baed297b7c0f18e77ae4cc1a3408167d
KallappaM <115877818+KallappaM@users.noreply.github.com>
2022-12-20 16:48:25 +0530

    Update BatteryHealthMonitor.c

--- BatteryHealthMonitor.c
+++ BatteryHealthMonitor.c
@@ -1,6 +1,11 @@
 #include "BatteryManagmentSystem.h"
 
 #define TOLERENCEVALUE 5
+#define SocBreachMin 20
+#define SocBreachMax 81
+#define SocWarningMin 24
+#define SocWarningMax 76
+
 #define RangeChecker(X,Y,Z) (X<=Y && X>=Z)?1:0
 
 struct 
@@ -54,7 +59,7 @@ WarningWithTolerance PreWarningIndicatorMessage(int input, int MinThreshold , in
 
 WarningRanges BatteryHelathMonitor(int soc)
 {  
-    if(RangeChecker(soc, 81, 21) == 1)
+    if(RangeChecker(soc, SocBreachMax, (SocBreachMin+1)) == 1)
 	{
 		return ReturnSocWarning(soc);
 	}
@@ -68,7 +73,7 @@ WarningRanges BatteryHelathMonitor(int soc)
 static WarningRanges ReturnSocBreach(int soc)
 {
 	 
-	if(SocRangeCheck(soc, 20, 81) == 1)
+	if(SocRangeCheck(soc, SocBreachMin, SocBreachMax) == 1)
 	{
 		return LOW_SOC_BREACH;
 	}
@@ -84,7 +89,7 @@ static WarningRanges ReturnSocWarning(int soc)
 	
 	WarningRanges Returnstaus = SOC_NORMAL;
 	
-	if(SocRangeCheck(soc, 24, 76) == 1)
+	if(SocRangeCheck(soc, SocWarningMin, SocWarningMax) == 1)
 	{
 		Returnstaus = LOW_SOC_WARNING;
 	}
