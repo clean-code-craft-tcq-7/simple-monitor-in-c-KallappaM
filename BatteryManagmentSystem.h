2765894ae004e6b5568a6f79c31d5975649ba2b3
KallappaM <115877818+KallappaM@users.noreply.github.com>
2022-12-22 14:39:20 +0530

    Update BatteryManagmentSystem.h

--- BatteryManagmentSystem.h
+++ BatteryManagmentSystem.h
@@ -27,5 +27,19 @@ typedef enum {
     Warning_max
 } WarningWithTolerance;
 
+const char* Store_WarningMessage[MESSAGE_MAX];
+/*Warning Message language table*/
+const char* WarningMessageTable[MAX_LANG][MESSAGE_MAX] = {
+		{/*Language : DEFAULT */
+			"LOW_SOC_BREACH","LOW_SOC_WARNING","SOC_NORMAL","HIGH_SOC_WARNING","HIGH_SOC_BREACH","SOC_UNDEFINED"},
+		{/*Language : ENGLISH */
+			"LOW_SOC_BREACH","LOW_SOC_WARNING","SOC_NORMAL","HIGH_SOC_WARNING","HIGH_SOC_BREACH","SOC_UNDEFINED"},
+		{/*Language : GERMAN */
+			"LOW_SOC_BREACH","LOW_SOC_WARNUNG","SOC_NORMAL","HIGH_SOC_WARNUNG","HIGH_SOC_BREACH","SOC_UNDEFINED"},
+		{ /*Language : CHINA */
+			"L_S_BREACHEN","L_S_WARN!!","NORMAL","H_S_WARN!!","H_S_BREACHEN","UNDEFINED"}
+		
+	};
+
 void SelectLanguageandWarnigMessage(Select_Language Language);
 WarningRanges BatteryHelathMonitor(int soc);
