99feb34e32d8497307289353b9a368d59cea1d82
KallappaM <115877818+KallappaM@users.noreply.github.com>
2022-12-22 15:55:25 +0530

    Update BatteryManagmentSystem.c

--- BatteryManagmentSystem.c
+++ BatteryManagmentSystem.c
@@ -41,19 +41,17 @@ int Check_ChargeRate(float chargeRate)
 
 int main()
 {
-  const char* rturnmessage;
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
   assert(batteryIsOk(44, 79, 0.7));
   assert(!batteryIsOk(46, 81, 0.8));
   assert(!batteryIsOk(0, 0, 0.7));
   SelectLanguageandWarnigMessage(DEFAULT);
-  rturnmessage = ReturnWarningMessage(13);
-  //assert(rturnmessage == "LOW_SOC_BREACH");
-  printf("%s\n",rturnmessage);
+  //assert(ReturnWarningMessage(13) == "LOW_SOC_BREACH");
+  printf("%s\n",ReturnWarningMessage(13));
   SelectLanguageandWarnigMessage(GERMAN);
-  printf("%s",ReturnWarningMessage(77));
   //assert(ReturnWarningMessage(77) == "HIGH_SOC_WARNUNG");
+  printf("%s",ReturnWarningMessage(77));
   assert(PreWarningMessageTable[PreWarningIndicatorMessage(23,SOCMIN,SOCMAX)] == "Approaching_discharge"); /* 5% of 80 is 4 , [20-24],[76-80] */
   assert(PreWarningMessageTable[PreWarningIndicatorMessage(77,SOCMIN,SOCMAX)] == "Approaching_charge_peak");
   assert(PreWarningMessageTable[PreWarningIndicatorMessage(1,TEMPMIN,TEMPMAX)] == "Approaching_discharge"); /* 5% of 45 is 2 , [0-2],[43-45] */
